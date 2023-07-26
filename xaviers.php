f77fa005e55694d8d63f48e5808583f32e8aea24
kaushalvikky006 <140623877+kaushalvikky006@users.noreply.github.com>
2023-07-26 19:18:16 +0530

    I have added new page

---
+++ xaviers.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Premium Series
+Description: A three-column, fixed-width blog design.
+Version    : 1.0
+Released   : 20090303
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>st xaviers</title>
+<meta name="keywords" content="" />
+<meta name="Premium Series" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="menu.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<!-- start header -->
+<?php
+include('inc/header1.php');
+
+
+
+
+?>
+<!-- end header -->
+<div id="wrapper">
+	<!-- start page -->
+	<div id="page">
+		<div id="sidebar1" class="sidebar">
+			<ul>
+				<li>
+					<h2>Our Facility</h2>
+					<ul>
+						<li><a href="hostel.php">Hostel</a></li>
+						<li><a href="transport.php">Transportation</a></li>
+						<li><a href="faculty.php">faculty</a></li>
+						<li><a href="artcraft.php">Art and Craft gallery</a></li>
+						<li><a href="musicroom.php">Music Room</a></li>
+						<li><a href="library.php">Library</a></li>
+						<li><a href="computerlab.php">Computer Lab</a></li>
+						<li><a href="biolab.php">Bio-lab</a></li>
+						<li><a href="chemistrylab.php">Chemistry Lab</a></li>
+						<li><a href="physicslab.php">Physics Lab</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2>Gallery</h2>
+					<ul>
+						<li><a href="#"></a>  <a href="#">Playground</a></li>
+						<li><a href="#"></a>  <a href="#">Vocational Function </a></li>
+						<li><a href="#"></a> <a href="#">Tour $ Travels</a></li>
+						<li><a href="#"></a> <a href="#">Campus</a></li>
+						<li><a href="#"></a> <a href="#">Faculty Room</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2>Categories</h2>
+					<ul>
+						<li><a href="#">Aliquam libero</a></li>
+						<li><a href="#">Consectetuer adipiscing elit</a></li>
+						<li><a href="#">Metus aliquam pellentesque</a></li>
+						<li><a href="#">Suspendisse iaculis mauris</a></li>
+						<li><a href="#">Urnanet non molestie semper</a></li>
+						<li><a href="#">Proin gravida orci porttitor</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2>Archives</h2>
+					<ul>
+					
+						
+						<li><a href="#">July</a> (31)</li>
+						<li><a href="#">August</a> (31)</li>
+						<li><a href="#">September</a> (30)</li>
+						<li><a href="#">October</a> (31)</li>
+						<li><a href="#">November</a> (23)</li>
+						<li><a href="#">December</a> (23)</li>
+						<li><a href="#">January</a> (23)</li>
+						<li><a href="#">February</a> (23)</li>
+						<li><a href="#">march</a> (23)</li>
+						<li><a href="#">april</a> (23)</li>
+						<li><a href="#">may</a> (23)</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!-- start content -->
+		<div id="content">
+			<div class="flower"><marquee direction="right"><img src="banner.png" alt="" width="510" height="250" /></marquee></div>
+			<div class="post">
+				<h1 class="title"><a href="kj.php">Welcome to Our St. Xaviers high School azamgarh Uttar Pradesh </a></h1>
+				<div class="entry">
+				<h2 class="title"> Mission Statement:-</h2>
+					<p><strong>The Mission of St. Xavier Catholic School</strong><a href="#"> is  to Educate the whole person-body,mind,and spirit_in cristian values in                      accordance with our catholic faith tradition</a>  </p>
+					
+				</div>
+			</div>
+			<div class="post">
+				<h2 class="title"><a href="#">Introduction:-</a></h2>
+				
+				<div class="entry">
+					
+					<blockquote>
+						<p><a href="#">The Diocese of Salina Catholic Schools is committed
+to providing an individual’s academic, spiritual,
+social, and physical growth. We seek to prepare
+students for responsible adult leadership in society,
+the world, and the church. St. Xavier Catholic School
+fosters growth in the Christian vocation by
+promoting awareness of the needs of society. As a
+school of excellence, we strive to prepare each
+young person for entrance into college or vocational
+education and subsequent employment and service
+to others. St. Xavier provides a variety of
+opportunities for students to gain experience in
+cooperating and competing with others.</a></p>
+					</blockquote>
+					<h3>Grading</h3>
+					<ul>
+						<P><a href="#">The academic year consists of four grading periods,
+two in each semester. Students will receive
+percentage grades each quarter of the school year:
+<li>92‐100% = A Excellent 4.0 grade points</li>
+<li>83‐91% = B Good 3.0 grade points</li>
+<li>74‐82% = C Satisfactory 2.0 grade points</li>
+<li>65‐73% = D Passing 1.0 grade point</li>
+<li>64% & below = F Failing 0.0 grade point</li>
+An incomplete will be given to a student only if they
+have excused absences. An incomplete for
+unfinished course requirements must be removed
+within two weeks of the end of the grading period or
+the grade will be changed to an “F” on school
+records, except in cases of prolonged illness or other
+circumstances approved by the principal and
+counselor. For KSHAA eligibility purposes,
+incompletes (I’s) are considered F’s.
+Semester grades are used to calculate a student’s
+cumulative grade and rank in the class. Only
+semester grades will be placed on a student’s</a></P>
+					</ul>
+					<h3>Discipline Policy:</h3>
+					<ol>
+						<p><a href="#">St. Xavier School has established a requirement for
+uniform dress for the following purpose:
+To promote an atmosphere of discipline and learning
+to enhance academic achievement;
+To value students for their character, by providing an
+equal socio‐economic footing for all;
+To foster the school’s sense of “community” by
+enhancing school pride and student self esteem.
+The approved uniform can be purchased from
+Parker School Uniforms
+Phone: (800) 500‐4634 Fax: (316) 687‐5238
+To obtain the website for online ordering use the
+following:
+https://www.parkersu.com/store/?school
+ID#=W1104163
+K‐5
+Girls –culottes, navy/khaki pants or shorts with
+green polo shirt, OR plaid jumper with white blouse.
+Boys – Navy or khaki pants or shorts with green polo
+shirt.
+2‐5 grades – Must wear a belt with pants or shorts. It
+is recommended for everyone to purchase a sweater
+or sweatshirt.</a></p>
+					</ol>
+					<p class="links"><a href="readmore.php" class="more">&laquo;&laquo;&nbsp;&nbsp;Read More&nbsp;&nbsp;&raquo;&raquo;</a></p>
+				</div>
+			</div>
+		</div>
+		<!-- end content -->
+		<!-- start sidebars -->
+		
+		<?php
+include('inc/config.php');
+
+
+if(isset($_POST['submit']))
+{
+//echo "lkhlkjl";exit;
+	$uname 	= $_POST['username'];
+	$pass 	= $_POST['password'];
+
+	if($uname !="" && $pass!=""){
+	
+	
+	echo $data 	= "SELECT * FROM user WHERE username = '".$uname."' AND password = '".$pass."'";
+	$res = mysql_query($data);
+	
+	
+	if(mysql_affected_rows() > 0)
+		{
+			$result = mysql_fetch_object($res);
+			$un = $result->username;
+			$p = $result->password;
+			
+			$_SESSION['user'] = $un;
+			
+			header("Location:student profile.php");
+		}
+		else{
+				header("Location: student profile.php?err=1 ");
+		}
+	
+	
+	
+	}
+	else
+	{
+		$msg = "<font>Please enter User Name or password";
+	
+	}
+
+}
+?>
+
+		<h2><a href="index2.php">Click Here To Registration For Student</a></h2>
+		
+		<div id="sidebar2" class="sidebar">
+			<ul>
+				<li>
+					<form id="searchform" method="post" action="#">
+						<div>
+							<h2>Click Here to Login</h2>
+							</div>
+							
+ 
+  <li>username</li>
+    <li><input type="text" name="username" value="" /></li>
+
+  
+    <li>Password</li>
+    <li><input type="password" name="password" value="" /></li>
+	    <li><input type="submit" name="submit" value="login" /></li>
+						
+					</form>
+			
+				<li>
+					<h2>Calendar</h2>
+					<div id="calendar_wrap">
+						<table summary="Calendar">
+							<caption>
+							may 2014
+							</caption>
+							<thead>
+								<tr>
+									<th abbr="Monday" scope="col" title="Monday">M</th>
+									<th abbr="Tuesday" scope="col" title="Tuesday">T</th>
+									<th abbr="Wednesday" scope="col" title="Wednesday">W</th>
+									<th abbr="Thursday" scope="col" title="Thursday">T</th>
+									<th abbr="Friday" scope="col" title="Friday">F</th>
+									<th abbr="Saturday" scope="col" title="Saturday">S</th>
+									<th abbr="Sunday" scope="col" title="Sunday">S</th>
+								</tr>
+							</thead>
+							<tfoot>
+								<tr>
+									<td abbr="September" colspan="3" id="prev"><a href="#" title="View posts for September 2009">&laquo; Sep</a></td>
+									<td class="pad">&nbsp;</td>
+									<td colspan="3" id="next">&nbsp;</td>
+								</tr>
+							</tfoot>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>2</td>
+									<td>3</td>
+									<td id="today">4</td>
+									<td>5</td>
+									<td>6</td>
+									<td>7</td>
+								</tr>
+								<tr>
+									<td>8</td>
+									<td>9</td>
+									<td>10</td>
+									<td>11</td>
+									<td>12</td>
+									<td>13</td>
+									<td>14</td>
+								</tr>
+								<tr>
+									<td>15</td>
+									<td>16</td>
+									<td>17</td>
+									<td>18</td>
+									<td>19</td>
+									<td>20</td>
+									<td>21</td>
+								</tr>
+								<tr>
+									<td>22</td>
+									<td>23</td>
+									<td>24</td>
+									<td>25</td>
+									<td>26</td>
+									<td>27</td>
+									<td>28</td>
+								</tr>
+								<tr>
+									<td>29</td>
+									<td>30</td>
+									<td>31</td>
+									<td class="pad" colspan="4">&nbsp;</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</li>
+				<li>
+					<h2>Categories</h2>
+					<ul>
+						<li><a href="#">Aliquam libero</a></li>
+						<li><a href="#">Consectetuer adipiscing elit</a></li>
+						<li><a href="#">Metus aliquam pellentesque</a></li>
+						<li><a href="#">Suspendisse iaculis mauris</a></li>
+						<li><a href="#">Urnanet non molestie semper</a></li>
+						<li><a href="#">Proin gravida orci porttitor</a></li>
+						<li><a href="#">Aliquam libero</a></li>
+						<li><a href="#">Consectetuer adipiscing elit</a></li>
+						<li><a href="#">Metus aliquam pellentesque</a></li>
+						<li><a href="#">Urnanet non molestie semper</a></li>
+						<li><a href="#">Proin gravida orci porttitor</a></li>
+						<li><a href="#">Aliquam libero</a></li>
+						<li><a href="#">Consectetuer adipiscing elit</a></li>
+						
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!-- end sidebars -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end page -->
+</div>
+<div id="footer">
+	<p class="copyright">&copy;&nbsp;&nbsp;2009 All Rights Reserved &nbsp;&bull;&nbsp;  <a href="http://www.freecsstemplates.org/"> this site is Design by Mr vivek yadav</a>.</p>
+	<p class="link"><a href="#">Privacy Policy</a>&nbsp;&#8226;&nbsp;<a href="#">Terms of Use</a></p>
+</div>
+</body>
+</html>
+
